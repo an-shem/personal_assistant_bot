8b53ec8898fb97c00179c08e1c4779663f4ab2d6
Ilona <ilona.gil@outlook.com>
2025-11-14 21:50:28 +0200

    C4.1 add prompt_toolkit

---
+++ src/utils/cli_input.py
@@ -0,0 +1,34 @@
+from prompt_toolkit import PromptSession
+from prompt_toolkit.history import FileHistory
+from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.styles import Style
+
+
+class Prompt:
+    """Universal input wrapper using prompt_toolkit with autocomplete & history."""
+
+    style = Style.from_dict({
+        "prompt": "bold #00afff",
+    })
+
+    def __init__(self, commands=None, history_file="command_history.txt"):
+
+        self.commands = commands or []
+        self.completer = WordCompleter(self.commands, ignore_case=True)
+        self.session = PromptSession(
+            history=FileHistory(history_file)
+        )
+
+    def ask(self, text=">>> ", enable_completion=True):
+        """
+        Custom prompt.
+        """
+        completer = self.completer if enable_completion else None
+
+        return self.session.prompt(
+            [("class:prompt", text)],
+            completer=completer,
+            style=self.style
+        )
+    
+
--- src/utils/handler_commands.py
+++ src/utils/handler_commands.py
@@ -1,6 +1,11 @@
 from src.models.notes_book import Note, NotesBook
 from src.utils.error_handler import input_error
 from src.models.address_book import AddressBook
+from src.utils.cli_input import Prompt
+
+
+ALL_COMMANDS = ["add-note", "show-notes", "delete-note", "edit-note", "exit", "help"] 
+PROMPT_TOOL = Prompt(commands=ALL_COMMANDS)
 
 @input_error
 def add_contact(args, book):
@@ -57,20 +62,25 @@ def birthdays(args, book: AddressBook):
     """Show contacts who have birthdays within the next 7 days."""
     pass
 
+
 @input_error
 def add_note(args, notes_book: NotesBook):
     """Add a new note with title and text."""
-    title = input("Enter a title:")
+    
+    title = PROMPT_TOOL.ask("Enter a title:", enable_completion=False).strip()
     if not title: 
         raise ValueError("The title of the note cannot be empty.")
     if title in notes_book.data: 
         return f"Note with title: '{title}' already exists."
-    content = input("Enter the text of the note:").strip()
+        
+    content = PROMPT_TOOL.ask("Enter the text of the note:", enable_completion=False).strip()
     if not content: 
         raise ValueError("The text of the note cannot be empty.")
+        
     note = Note(title, content)
     notes_book.add_note(note)
     return f"Note '{title}' successfully added."
+
 
 @input_error
 def show_notes(args, notes_book: NotesBook):
@@ -80,19 +90,29 @@ def show_notes(args, notes_book: NotesBook):
 @input_error
 def find_note(args, notes_book: NotesBook):
     """Find notes by keyword in title or content."""
-    if len(args) != 1:
-        raise ValueError("Usage: find-note [keyword]") 
-    keyword = args[0]
+    if len(args) == 0:
+        keyword = PROMPT_TOOL.ask("Enter keyword to search:", enable_completion=False).strip() 
+        if not keyword:
+            raise ValueError("Keyword cannot be empty.")
+    elif len(args) == 1:
+        keyword = args[0]
+    else:
+        raise ValueError("Usage: find-note [keyword]")
+
     found_notes = notes_book.search_notes_by_keyword(keyword)
+    
     if not found_notes:
         return f"No notes found containing '{keyword}'."
+        
     return "---Notes found---\n" + "\n\n".join(str(n) for n in found_notes)
+
 
 @input_error
 def edit_note(args, notes_book: NotesBook):
     """Edit an existing note by title."""
-    title = input("Enter a title for change: ")
-    new_content = input("Enter new content: ")
+    title = PROMPT_TOOL.ask("Enter a title for change: ", enable_completion=False).strip() 
+
+    new_content = PROMPT_TOOL.ask("Enter new content: ", enable_completion=False).strip()
 
     note = notes_book.find_note_by_title(title)
 
@@ -108,7 +128,7 @@ def edit_note(args, notes_book: NotesBook):
 @input_error
 def delete_note(args, notes_book: NotesBook):
     """Delete a note by title."""
-    title = input("Enter a title for delete: ").strip() 
+    title = PROMPT_TOOL.ask("Enter a title for delete: ", enable_completion=False).strip() 
     
     if not title:
         raise ValueError("The note name cannot be empty.")
